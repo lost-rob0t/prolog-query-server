30a403681a04b0ff9b4bea21dfbfc1ca2bffc7d7
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-11 21:38:49 -0400

    Run structured proof tree unit tests

--- tests/run.pl
+++ tests/run.pl
@@ -2,6 +2,7 @@
 
 main :-
     load_files(['expert_system_tests.pl',
+                'proof_tree_tests.pl',
                 'query_server_protocol_tests.pl'],
                [silent(true)]),
     (   run_tests
