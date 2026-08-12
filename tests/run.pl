2d72eb0ed91519a8a474c44363bb04aaeea700c3
nsaspy <104283403+lost-rob0t@users.noreply.github.com>
2026-08-11 21:59:59 -0400

    Run knowledge-base static analysis tests

--- tests/run.pl
+++ tests/run.pl
@@ -4,6 +4,7 @@ main :-
     load_files(['expert_system_tests.pl',
                 'proof_tree_tests.pl',
                 'builtin_tests.pl',
+                'kb_analysis_tests.pl',
                 'query_server_protocol_tests.pl'],
                [silent(true)]),
     (   run_tests
